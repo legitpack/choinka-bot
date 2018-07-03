e20c65ee5e57663f82b0a38e845dc87ecc22f4c5
legitpack <40607342+legitpack@users.noreply.github.com>
2018-07-04 01:35:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
-    client.user.setStatus('idle')
-    client.user.setPresence({
-        game: {
-            name: 'HIMC.PL',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/kissing_my_depp3881"
-        }
-    });
+  console.log(`Logged in as ${client.user.username}!`);
+    client.user.setStatus('dnd')
+});
+
+client.on('message', (message) => {
+    if(message.content == '--help') {
+        message.channel.send('we pal stond gume :heart:');
+    }
+});
+
+client.on("guildCreate", guild => {
+  console.log(`Dołączono do: ${guild.name} (id: ${guild.id}). Serwer posiada ${guild.memberCount} czlonkow!`);
+  client.user.setActivity(`--help || Servers: ${client.guilds.size}`);
+});
+  
+client.on("guildDelete", guild => {
+  console.log(`Wyrzucono mnie z: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`--help || Servers: ${client.guilds.size}`)
 });
 
 client.login("NDYzNjgzNDE3NjUzOTAzMzcw.Dhz-XA.Zo6I56DRs0cpI7JUgiHH8ZyoWFE");
