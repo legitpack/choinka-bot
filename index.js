32812655bd3ba514911a840b12fcb7e1d1d119ee
legitpack <40607342+legitpack@users.noreply.github.com>
2018-07-04 01:39:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
-    client.user.setStatus('dnd')
+    client.user.setStatus('idle')
 });
 
 client.on('message', (message) => {
